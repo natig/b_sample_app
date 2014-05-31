ab8bc20499cde07e261dd0304448f71fe8a06bb6
natig <natig1906@ya.ru>
2014-05-31 22:05:53 +0400

    Finish user signup

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,10 +3,14 @@ require 'spec_helper'
 describe User do
  
   before do
-    @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
+    @user = User.new(name: "Example User",
+                     email: "user@example.com",
+                     password: "foobar",
+                     password_confirmation: "foobar")
   end
 
   subject { @user }
+ 
   # валидация наличия атрибутов
   it { should respond_to(:name) }
   it { should respond_to(:email) }
