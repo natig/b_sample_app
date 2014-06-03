9a7342ccb299065596652becb46a335a8fb6d164
natig <natig1906@ya.ru>
2014-06-03 20:09:25 +0400

    9.2.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,7 +50,7 @@ class UsersController < ApplicationController
     end
 
     def correct_user
-      @user = User.find.find(params[:id])
+      @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
     end
 end
