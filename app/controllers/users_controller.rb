96b017d454d12c44dddeed8178d2e2b2fb2ddb71
natig <natig1906@ya.ru>
2014-06-04 14:59:02 +0400

    (no error)->9.3.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:edit, :update]
+  before_action :signed_in_user, only: [:index, :edit, :update]
   before_action :correct_user,   only: [:edit, :update]
+
+  def index
+    @users = User.all
+  end
 
   def show
     @user = User.find(params[:id])
@@ -26,7 +30,6 @@ class UsersController < ApplicationController
   end
 
   def update
-   # @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       flash[:success] = "Profile updated"
       redirect_to @user
@@ -39,7 +42,7 @@ class UsersController < ApplicationController
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
-                                          :password_confirmation)
+                                   :password_confirmation)
     end
 
     # Before filters
