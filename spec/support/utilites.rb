9a6943d79cf81f71e17e4c13e31a3f29c999352e
natig <natig1906@ya.ru>
2014-07-24 20:48:43 +0400

    optimization code this file: authentication_pages_spec.rb

--- spec/support/utilites.rb
+++ spec/support/utilites.rb
@@ -21,8 +21,6 @@ def sign_in(user, options={})
     user.update_attribute(:remember_token, User.encrypt(remember_token))
   else
     visit signin_path
-    fill_in "Email",    with: user.email
-    fill_in "Password", with: user.password
-    click_button "Sign in"
+    valid_signin(user)
   end
 end
